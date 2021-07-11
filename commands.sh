0adb36163f59b87ccc956055215ca2ca52fa89c8
Shariq Mustaquim <shariq.mustquim@zand.ae>
2021-07-11 15:55:11 +0400

    latest updates to github action workflow

--- commands.sh
+++ commands.sh
@@ -12,3 +12,24 @@ $ kubectl create -f jenkins-service.yaml --namespace jenkins
 $ kubectl get services --namespace jenkins
 
 # From: https://levelup.gitconnected.com/running-jenkins-inside-a-kubernetes-cluster-bd86822d487
+
+
+# Ingress
+
+# Create a namespace for your ingress resources
+kubectl create namespace ingress-basic
+
+# Add the ingress-nginx repository
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+# Use Helm to deploy an NGINX ingress controller
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace ingress-basic \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
+
+# Check
+$ kubectl --namespace ingress-basic get services -o wide -w nginx-ingress-ingress-nginx-controller
+
