af419902ec3bf99ca3fdce4148cd290f0f721822
Shariq Mustaquim <shariq.mustquim@zand.ae>
2021-07-11 14:51:56 +0400

    latest updates to github action workflow

--- commands.sh
+++ commands.sh
@@ -2,15 +2,13 @@
 
 
 
-
+# Only for Jenkins in an AKS Cluster
 
 $ kubectl create namespace jenkins
-$ kubectl create -f jenkins-storage.yaml -n jenkins
 $ kubectl create -f jenkins-pvc.yaml -n jenkins
 $ kubectl create -f jenkins-deployment.yaml -n jenkins
 $ kubectl get pods -n jenkins
 $ kubectl create -f jenkins-service.yaml --namespace jenkins
 $ kubectl get services --namespace jenkins
 
-From: https://levelup.gitconnected.com/running-jenkins-inside-a-kubernetes-cluster-bd86822d487
-
+# From: https://levelup.gitconnected.com/running-jenkins-inside-a-kubernetes-cluster-bd86822d487
