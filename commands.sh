67d54959200e44d12c93fb3328d3631d859f6503
Shariq Mustaquim <shariq.mustquim@zand.ae>
2021-07-11 14:26:46 +0400

    latest updates to github action workflow

--- commands.sh
+++ commands.sh
@@ -1,27 +1,16 @@
 
+
+
+
+
+
 $ kubectl create namespace jenkins
-
-$ vi jenkins-pvc.yaml
-
+$ kubectl create -f jenkins-storage.yaml -n jenkins
 $ kubectl create -f jenkins-pvc.yaml -n jenkins
-
-$ vi jenkins-deployment.yaml 
-
 $ kubectl create -f jenkins-deployment.yaml -n jenkins
-
 $ kubectl get pods -n jenkins
-
-$ vi jenkins-service.yaml
-
 $ kubectl create -f jenkins-service.yaml --namespace jenkins
-
 $ kubectl get services --namespace jenkins
-
-
-
-
-
-
 
 From: https://levelup.gitconnected.com/running-jenkins-inside-a-kubernetes-cluster-bd86822d487
 
